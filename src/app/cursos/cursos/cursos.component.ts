fbb680ef0ab0c9e7e11e4139eb7c54366c80deca
Aristídes Debortoli Neto <netodebortoli77@gmail.com>
2023-04-08 22:43:37 -0300

    Alterado modulo de serviços e componente do cursos. Métod de listar cursos, na camada de CursosService, irá retonar uma lista de Obersavable, do tipo Cursor, receendo esses dados vit método GET http.

--- src/app/cursos/cursos/cursos.component.ts
+++ src/app/cursos/cursos/cursos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cursos } from '../model/cursos';
 import { CursosService } from '../services/cursos.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-cursos',
@@ -9,13 +10,13 @@ import { CursosService } from '../services/cursos.service';
 })
 export class CursosComponent implements OnInit {
 
-  cursos: Cursos[] = [];
+  cursos: Observable<Cursos[]>
   displayedColumns = ['nome', 'categoria'];
 
   constructor(private cursoServico: CursosService) {
+    this.cursos = this.cursoServico.list();
   }
 
   ngOnInit(): void {
-    this.cursos = this.cursoServico.list();
   }
 }
--- src/app/cursos/services/cursos.service.ts
+++ src/app/cursos/services/cursos.service.ts
@@ -1,18 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
 import { Cursos } from '../model/cursos';
+
+import { first, tap } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CursosService {
 
+  private readonly API = '/assets/cursos.json';
+
   constructor(private httpClient: HttpClient) { }
 
-  list(): Cursos[] {
-    return [
-      { _id: '1', nome: 'Java Script', categoria: 'Front-End'}
-    ]
+  list() {
+    return this.httpClient.get<Cursos[]>(this.API).
+    pipe(
+      first(),
+        tap(cursos => console.log(cursos))
+    )
   }
 }
